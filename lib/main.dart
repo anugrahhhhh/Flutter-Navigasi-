4498a7c211a2cabe77f6aacfd78405cc1fd4acb1
anugrahhhhh <anugrahputa270@gmail.com>
2025-04-08 20:09:53 +0700

    Belajar Navigasi

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+// //1
+// import 'package:flutter/material.dart';
+// import 'page_one.dart';
+
+// void main() {
+//   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: PageOne()));
+// }
+
+//2
+// import 'package:flutter/material.dart';
+// import 'home_with_drawer.dart'; // Pastikan nama file dan path sesuai
+
+// void main() {
+//   runApp(
+//     MaterialApp(debugShowCheckedModeBanner: false, home: HomeWithDrawer()),
+//   );
+// }
+
+//3
+import 'package:flutter/material.dart';
+import 'tab_navigation.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: TabNavigation()));
+}
